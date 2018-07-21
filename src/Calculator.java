636616dbf041daf49a0936296db9b1c1b0dfdc43
philliphjhuang <league@WTS22.attlocal.net>
2018-07-20 18:26:57 -0700

    book of illusion, fortune teller, calculator
    .

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,7 @@ import javax.swing.JTextField;
 
 public class Calculator {
 	JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
+	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	JButton add = new JButton();
 	JButton sub = new JButton();
@@ -14,24 +14,26 @@ public class Calculator {
 	JButton div = new JButton();
 	JTextField num1 = new JTextField();
 	JTextField num2 = new JTextField();
-	
-	
-	
-	
-public void makeCalculator() {
-	JFrame frame = new JFrame();
-	frame.setVisible(true);
-	frame.add(panel);
-	frame.setSize(400, 150);
-	frame.setTitle("Simple Calculator");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	
-	
-	
-}
+	double num = 0;
+	String numm;
 
-public static void main(String[] args) {
-new Calculator().makeCalculator();
-}
+	public void makeCalculator() {
+
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setSize(400, 200);
+		frame.setTitle("Simple Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		numm = Double.toString(num);
+		label.setText(numm);
+		frame.add(label);
+		num = Double.parseDouble(numm);
+
+	}
+
+	public static void main(String[] args) {
+		new Calculator().makeCalculator();
+	}
 }
--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -21,111 +21,131 @@ import javax.swing.SwingUtilities;
 
 public class FortuneTeller extends JPanel implements Runnable, MouseListener {
 
-    JFrame frame = new JFrame();
+	JFrame frame = new JFrame();
 
-    int frameWidth = 500;
-    int frameHeight = 400;
+	int frameWidth = 250;
+	int frameHeight = 250;
 
-    FortuneTeller() throws Exception {
-   	 // 1. Choose an image for your fortune teller and put it in your default package
-   	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
-   	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
-   	 // 4. add a mouse listener to the frame
+	FortuneTeller() throws Exception {
+		// 1. Choose an image for your fortune teller and put it in your default package
+		fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
+		// 2. Adjust the frameWidth and frameHeight variables to fit your image nicely
+		// (doesn’t need a new line of code)
 
-    }
+		// 4. add a mouse listener to the frame
+		frame.addMouseListener(this);
+	}
 
-    static void begin() {
-   	 // 3. Welcome the user. Give them a hint for the secret location.
+	static void begin() {
+		// 3. Welcome the user. Give them a hint for the secret location.
+		JOptionPane.showMessageDialog(null, "Welcome eson");
 
-    }
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-   	 int mouseX = e.getX();
-   	 int mouseY = e.getY();
-   	 // 5. Print the mouseX variable
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+		// 5. Print the mouseX variable
+		System.out.println("(" + mouseX + ", " + mouseY + ")");
+		// 6. Add the mouseY variable to the previous line so that it prints out too (no
+		// new line)
 
-   	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
-   	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
-   	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
-   	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
-   		 // 8. Get the user to enter a question for the fortune teller
+		// 7. Adjust your secret location co-ordinates here:
+		int secretLocationX = 125;
+		int secretLocationY = 125;
+		/**
+		 * If the mouse co-ordinates and secret location are close, we'll let them ask a
+		 * question.
+		 */
+		if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
+			// 8. Get the user to enter a question for the fortune teller
+			JOptionPane.showInputDialog("Ask me a question");
+			// 9. Find a spooky sound and put it in your default package
+			// (freesound.org)dsaasddsaasddddasdfffdasasdffdsaadsffdsaasddffdsaasdffdsa
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource("demacia.wav"));
+			// 10. Play the sound
+			sound.play();
+			// 11. Use the pause() method below to wait until your music has finished
+			pause(4);
+			// 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
+			Random r = new Random();
+			int y = r.nextInt(4);
+			System.out.println(y);
+			JOptionPane.showInputDialog("Ask a question for the 8 ball");
+			if (y == 0) {
+				JOptionPane.showMessageDialog(null, "Yes");
+			} else if (y == 1) {
+				JOptionPane.showMessageDialog(null, "No");
+			} else if (y == 2) {
+				JOptionPane.showMessageDialog(null, "Maybe you should go ask Google");
+			} else if (y == 3) {
+				JOptionPane.showMessageDialog(null, "Deep Dark Fantasies");
+			}
+		}
+	}
 
-   		 // 9. Find a spooky sound and put it in your default package (freesound.org)
-   		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
-   		 // 10. Play the sound
+	private boolean areClose(int mouseX, int secretLocationX) {
+		return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
+	}
 
-   		 // 11. Use the pause() method below to wait until your music has finished
+	private void pause(int seconds) {
+		try {
+			Thread.sleep(1000 * seconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-   		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
+	/**************** don't worry about the stuff under here *******************/
 
-   	 }
+	BufferedImage fortuneTellerImage;
 
-    }
+	public static void main(String[] args) throws Exception {
+		SwingUtilities.invokeLater(new FortuneTeller());
+		begin();
+	}
 
-    private boolean areClose(int mouseX, int secretLocationX) {
-   	 return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
-    }
+	@Override
+	public void run() {
+		frame.add(this);
+		setPreferredSize(new Dimension(frameWidth, frameHeight));
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 
-    private void pause(int seconds) {
-   	 try {
-   		 Thread.sleep(1000 * seconds);
-   	 } catch (InterruptedException e) {
-   		 e.printStackTrace();
-   	 }
-    }
-    
-    /**************** don't worry about the stuff under here *******************/
-    
-    BufferedImage fortuneTellerImage;
+	private void showAnotherImage(String imageName) {
+		try {
+			fortuneTellerImage = ImageIO.read(getClass().getResource(imageName));
+		} catch (Exception e) {
+			System.err.println("Couldn't find this image: " + imageName);
+		}
+		repaint();
+	}
 
-    public static void main(String[] args) throws Exception {
-   	 SwingUtilities.invokeLater(new FortuneTeller());
-   	 begin();
-    }
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(fortuneTellerImage, 0, 0, null);
+	}
 
-    @Override
-    public void run() {
-   	 frame.add(this);
-   	 setPreferredSize(new Dimension(frameWidth, frameHeight));
-   	 frame.pack();
-   	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   	 frame.setResizable(false);
-   	 frame.setVisible(true);
-    }
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
 
-private void showAnotherImage(String imageName) {
-   	 try {
-   		 fortuneTellerImage = ImageIO.read(getClass().getResource(imageName));
-   	 } catch (Exception e) {
-   		 System.err.println("Couldn't find this image: " + imageName);
-   	 }
-   	 repaint();
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-   	 g.drawImage(fortuneTellerImage, 0, 0, null);
-    }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
 
-    @Override
-    public void mouseClicked(MouseEvent e) {}
-
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-
-    @Override
-    public void mouseExited(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
 
 }
 
 // Copyright The League, 2016
-
-
-
-
